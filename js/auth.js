45acb9ce1fd7ee519fa16c9719a5f52a123a1463
Doedie2003 <145853966+Doedie2003@users.noreply.github.com>
2026-09-02 11:40:28 +0200

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,1 +1,21 @@
+async function login() {
 
+    const email = document.getElementById("email").value.trim();
+    const password = document.getElementById("password").value;
+
+    const { data, error } = await supabaseClient.auth.signInWithPassword({
+        email,
+        password
+    });
+
+    if (error) {
+        document.getElementById("loginMessage").innerText =
+            "Fout: " + error.message;
+        console.log(error);
+        return;
+    }
+
+    window.location.href = "dashboard.html";
+}
+
+document.getElementById("loginButton").addEventListener("click", login);
