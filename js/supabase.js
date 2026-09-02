95d26a51025d53fd5564731fab839bbcfe4ce639
Doedie2003 <145853966+Doedie2003@users.noreply.github.com>
2026-09-02 11:41:52 +0200

    Update supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -1,1 +1,7 @@
+const SUPABASE_URL = "https://eextosoasyuwgbpqpikm.supabase.co";
+const SUPABASE_KEY = "sb_publishable_fvKGimojsI1dWWb9Y8gMIg_qO_QAmJR";
 
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
